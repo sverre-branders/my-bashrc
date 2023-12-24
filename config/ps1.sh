ebfbf69c9756a7dd32751bd8dd64dc3c642bb6be
sverre-branders <sverre.branders@protonmail.com>
2023-12-24 10:55:19 +0100

    Better ps1

--- config/ps1.sh
+++ config/ps1.sh
@@ -1,54 +1,74 @@
-RESET="\[\033[00m\]"
-fgWHITE="\[\033[1;38;5;254m\]"
-fgSEC="\[\033[1;38;5;2m\]"
-fgDARK_GREY="\[\033[38;5;239m\]"
+# Color Definitions
+WHITE="254"
+DARK="239"
+MAIN="4"
+SEC="2"
 
-bgDARK_GREY="\[\033[48;5;239m\]"
-bgMAIN="\[\033[48;5;4m\]"
-bgSEC="\[\033[48;5;2m\]"
-
-parse_git_branch() {
-    if [ -d "$(git rev-parse --git-dir 2>/dev/null)" ]; then
-        branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-        commit=$(git rev-parse --short HEAD)
-        if [ -n "$branch" ]; then
-            echo " [ $branch - $commit ] "
-        else
-            echo " [ $commit ] "
-        fi
-    else
-        echo ''
-    fi
+FG() {
+    case $1 in
+        bWhite) echo -n "\[\033[1;38;5;${WHITE}m\]";;
+        bMain) echo -n "\[\033[1;38;5;${MAIN}m\]";;
+        bSec) echo -n "\[\033[1;38;5;${SEC}m\]";;
+        bDark) echo -n "\[\033[1;38;5;${DARK}m\]";;
+        white) echo -n "\[\033[0;38;5;${WHITE}m\]";;
+        main) echo -n "\[\033[0;38;5;${MAIN}m\]";;
+        sec) echo -n "\[\033[0;38;5;${SEC}m\]";;
+        dark) echo -n "\[\033[0;38;5;${DARK}m\]";;
+        *) echo -n "\[\033[0;39m\]";;
+    esac
 }
 
-parse_git_status() {
-    if [ -d "$(git rev-parse --git-dir 2>/dev/null)" ]; then
-        if [[ -z $(git status -s) ]]; then
-            echo ''
-        else
-            echo " [+]"
-        fi
-    else
-        echo ''
+BG() {
+    case $1 in
+        white) echo -n "\[\033[48;5;${WHITE}m\]";;
+        main) echo -n "\[\033[48;5;${MAIN}m\]";;
+        sec) echo -n "\[\033[48;5;${SEC}m\]";;
+        dark) echo -n "\[\033[48;5;${DARK}m\]";;
+        *) echo -n "\[\033[49m\]";;
+    esac
+}
+
+CMD_symbol=$(echo -n -e "\u2B9E")
+SEP=$(echo -n -e "\uE0B0\u2B9E")
+RESET="$(echo -n -e "\[\033[0m\]")"
+
+parse_git() {
+    # Check whether in git repo
+    if ! git rev-parse --is-inside-git-dir >/dev/null 2>&1; then
+        return
     fi
+
+    # Get info
+    local commit="$(git log --pretty=format:'%h' -n 1)"
+    local branch="$(git branch 2>/dev/null | grep '^\*' | sed -e "s/^* //")"
+    local modified=0
+    while read -r line; do
+        if [[ "$line" =~ ^_._[^[:space:]]_ ]]; then
+            modified=1
+        fi
+    done < <(git status --short | cut -b -2 | sed -e 's/\(.\)\(.*\)/_\1_\2_/')
+
+    # Print Status
+    local string=""
+    if [ $modified -ne 0 ]; then
+        string+=" [+]"
+    fi
+    string+=" [ $branch - $commit ] "
+    echo -n "$string"
 }
 
 parse_time() {
-    echo " [ $(date +%T) ] "
+    echo -n " [ $(date +%T) ] "
 }
 
-parse_conda_env() {
-  env_name=$(conda info --env | grep '^active environment' | awk '{print $4}')
-  if [ -n "$env_name" ]; then
-    echo " ($env_name)"
-  else
-    echo ""
-  fi
-}
+PS1=""
+PS1+="$(BG sec)$(FG bWhite)\$(parse_git)${RESET}" # Git status
+PS1+="$(FG sec)$(BG dark)$SEP${RESET}" # Separator
 
-separator() {
-    echo -e $'\uE0B0'
-}
+PS1+="$(BG dark)$(FG bWhite) \u@\h ${RESET}" # User info
+PS1+="$(FG dark)$SEP${RESET}" # Separator
 
-export PS1="${fgWHITE}${bgSEC}\$(parse_git_status)\$(parse_git_branch)${fgSEC}${bgDARK_GREY}\$(separator)${RESET}${bgDARK_GREY} \u@\h ${fgWHITE}[ \W ] ${RESET}${fgDARK_GREY}\$(separator)${RESET} \$(date +%T) ${fgWHITE}$ ${RESET}"
-export PS2="${fgSEC}~${RESET} "
+PS1+="$(FG white) [ $(date +%T) ] ${RESET}" # Time stamp
+PS1+="$CMD_symbol "
+
+
