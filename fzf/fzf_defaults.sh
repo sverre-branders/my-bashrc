4faf6f03095d0a46316b46b5eb088632bf54bf85
sverre-branders <sverre.branders@protonmail.com>
2024-01-23 16:39:30 +0100

    updated fzf default config

--- fzf/fzf_defaults.sh
+++ fzf/fzf_defaults.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
  
-export FZF_DEFAULT_OPTS="--border rounded --layout reverse --pointer ⮞ --marker '*' --prompt '' --color 'hl:2,marker:white,pointer:white,spinner:2,info:grey,gutter:-1,bg+:-1'"
+export FZF_DEFAULT_OPTS="--border sharp --layout reverse --pointer ⮞ --marker '*' --prompt '' --color 'hl:2,marker:white,pointer:white,spinner:2,info:grey,gutter:-1,bg+:-1,hl+:2'"
