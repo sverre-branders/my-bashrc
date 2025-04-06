a735d0cbd5f751ef90218057b947f5c85e88480d
sverre-branders <sverre.branders@protonmail.com>
2025-04-06 19:50:20 +0200

    Added git commit log search function and bindings to defaults

--- fzf/fzf_defaults.sh
+++ fzf/fzf_defaults.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
  
-export FZF_DEFAULT_OPTS="--border sharp --layout reverse --pointer ⮞ --marker '*' --prompt '' --color 'hl:2,marker:white,pointer:white,spinner:2,info:grey,gutter:-1,bg+:-1,hl+:2'"
+export FZF_DEFAULT_OPTS="--border sharp --layout reverse --pointer ⮞ --marker '*' --prompt '' --color 'hl:2,marker:white,pointer:white,spinner:2,info:grey,gutter:-1,bg+:-1,hl+:2' --bind 'alt-j:down,alt-k:up,alt-J:preview-down,alt-K:preview-up,ctrl-d:preview-page-down,ctrl-u:preview-page-up,alt-q:abort,ctrl-m:execute:'
+"
--- fzf/key-bindings.bash
+++ fzf/key-bindings.bash
@@ -113,3 +113,16 @@ bind -m vi-insert '"\eb": "\C-z\ec\C-z"'
 source "${bashrc_path%/bashrc}/fzf/scripts/select_audio_device.sh"
 
 fi
+
+# CUSTOM: git commit log
+git-commit-show ()
+{
+  git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr"  | \
+   fzf --ansi --no-sort --reverse --tiebreak=index --preview \
+   'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
+   --bind "alt-j:down,alt-k:up,alt-J:preview-down,alt-K:preview-up,ctrl-d:preview-page-down,ctrl-u:preview-page-up,alt-q:abort,ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF" --preview-window=right:60%
+}
